2d3e9979b3444912e4224afae7992e9564656c75
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2014-10-20 08:24:31 +0200

    Conversion into web service

--- src/main/java/org/jorge/twitterpromoter/Main.java
+++ src/main/java/org/jorge/twitterpromoter/Main.java
@@ -1,5 +1,7 @@
 package org.jorge.twitterpromoter;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.jorge.twitterpromoter.io.Ticker;
 
 public class Main {
@@ -8,13 +10,24 @@ public class Main {
      * @param args The args to the main method
      */
     public static void main(String[] args) throws Exception {
+        String webPort = System.getenv("PORT");
+        if ((webPort == null) || webPort.isEmpty()) {
+            webPort = "8080";
+        }
 
-        Thread dummyThreadThatWillNeverStart = new Thread();
+        Server server = new Server(Integer.valueOf(webPort));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        System.out.print("Requesting server start...");
+        server.start();
+        System.out.println("done.");
 
         System.out.println("Before initing");
         Ticker.getInstance().init();
         System.out.println("After initing");
 
-        dummyThreadThatWillNeverStart.join();
+        server.join();
     }
 }
